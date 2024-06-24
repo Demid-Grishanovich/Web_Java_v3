bae9385830c59cfdc945b1988e520a03613fd262
Demid-Grishanovich <demidgrishs@gmail.com>
2024-06-24 06:23:08 +0300

    13

--- src/main/java/com/example/v3/config/DatabaseConfig.java
+++ src/main/java/com/example/v3/config/DatabaseConfig.java
@@ -1,13 +1,17 @@
 package com.example.v3.config;
 
-import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
 
 public class DatabaseConfig {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/Jakarta";
     private static final String USERNAME = "postgres";
     private static final String PASSWORD = "root";
+
+    private static BasicDataSource dataSource;
+
     static {
         try {
             Class.forName("org.postgresql.Driver");
@@ -15,20 +19,18 @@ public class DatabaseConfig {
             e.printStackTrace();
         }
     }
-    private static DataSource dataSource;
 
     public static DataSource getDataSource() {
         if (dataSource == null) {
             synchronized (DatabaseConfig.class) {
                 if (dataSource == null) {
-                    BasicDataSource ds = new BasicDataSource();
-                    ds.setUrl(URL);
-                    ds.setUsername(USERNAME);
-                    ds.setPassword(PASSWORD);
-                    ds.setMinIdle(5);
-                    ds.setMaxIdle(10);
-                    ds.setMaxOpenPreparedStatements(100);
-                    dataSource = ds;
+                    dataSource = new BasicDataSource();
+                    dataSource.setUrl(URL);
+                    dataSource.setUsername(USERNAME);
+                    dataSource.setPassword(PASSWORD);
+                    dataSource.setMinIdle(5);
+                    dataSource.setMaxIdle(10);
+                    dataSource.setMaxOpenPreparedStatements(100);
                 }
             }
         }
